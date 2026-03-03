6c41ddab58c54c932de6f3a753f271cc271661de
andrew-miroiu <andrew.miroiu@gmail.com>
2026-03-03 15:00:22 +0200

    put frontend cors link

--- springboot-todo-api/src/main/java/com/andrei/springboot/security/SecurityConfig.java
+++ springboot-todo-api/src/main/java/com/andrei/springboot/security/SecurityConfig.java
@@ -49,7 +49,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173",
+            "https://onlyfriendsjavaedition.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
