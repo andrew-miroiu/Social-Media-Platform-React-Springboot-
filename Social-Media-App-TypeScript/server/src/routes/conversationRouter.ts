41b38e9b97953eff541f03082bc6f94e17cb04ae
andrew-miroiu <andrew.miroiu@gmail.com>
2026-03-03 03:20:35 +0200

    deleted node server

--- Social-Media-App-TypeScript/server/src/routes/conversationRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createClient } from "@supabase/supabase-js";
-import { createConversation } from "../controllers/conversationController";
-
-const router = express.Router();
-
-router.post("/createConversation", createConversation);
-
-export default router;
